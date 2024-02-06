c93d685c99a0510a35de349f4b8fcfcbfa49b747
shivamrohela <119654333+shivamrohela@users.noreply.github.com>
2024-02-06 19:19:06 +0000

    datatypes

---
+++ .devcontainer/1_basics/datatypes.js
@@ -0,0 +1,13 @@
+/*
+
+BigInt 5563663222 its range more then number datatypes 
+Number 25854 its range 2 power 52 something
+Boolean => true or flase
+null => typeof = object
+undefined => typeof = undefined
+String => "" , ''
+
+symbol => unique
+
+object
+*/
---
+++ .devcontainer/1_basics/variables.js
@@ -0,0 +1,13 @@
+const nums = 100;
+let names = "shivam";
+var age = 25;
+address = "moradabad";
+
+let news;
+
+//nums = 50;
+names = "rohela";
+age = 24;
+address = "mbd";
+
+console.table([nums, names, age, address, news]); 
