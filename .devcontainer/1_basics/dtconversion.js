be3d63cd2feaab63b358a5174834b2d1537285d7
shivamrohela <119654333+shivamrohela@users.noreply.github.com>
2024-02-07 12:57:29 +0000

    comparison operator

--- .devcontainer/1_basics/dtconversion.js
+++ .devcontainer/1_basics/dtconversion.js
@@ -9,3 +9,24 @@ console.log(typeof(tostring));
 //null typeof => object
 //undefined typeof => undefined
 //NaN typeof => number 
+
+/*
+If used postfix, with operator after operand (for example, x++), 
+the increment operator increments and returns the value before incrementing.
+*/
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+//Expected output: "x:4, y:3"
+
+//If used prefix, with operator before operand (for example, ++x), 
+//the increment operator increments and returns the value after incrementing.
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+//Expected output: "a:4, b:4"
+
+
