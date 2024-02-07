0dc2e181a51a252af741d075d366850e1514e340
shivamrohela <119654333+shivamrohela@users.noreply.github.com>
2024-02-07 06:15:42 +0000

    conversion of datatypes

---
+++ .devcontainer/1_basics/dtconversion.js
@@ -0,0 +1,11 @@
+let num = NaN
+
+console.log(typeof(num));
+
+let tostring = String(num);
+
+console.log(typeof(tostring));
+
+//null typeof => object
+//undefined typeof => undefined
+//NaN typeof => number 
