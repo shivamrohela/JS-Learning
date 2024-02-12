f87a51b1b93b70058a68d7d26b14f4bc15e9f1a4
shivamrohela <119654333+shivamrohela@users.noreply.github.com>
2024-02-12 07:32:56 +0000

    numbers and math

---
+++ .devcontainer/1_basics/number_maths.js
@@ -0,0 +1,39 @@
+const num = 100
+// console.log(num);
+
+const num1 = new Number(100.3456);
+// console.log(typeof(num1));
+
+// console.log(num1.toString());
+
+// console.log(num1.toFixed(1));
+
+// console.log(num1.toPrecision(4));
+
+const num2 = 1000000000;
+// console.log(num2.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.455));
+// console.log(Math.ceil(5.455));
+// console.log(Math.floor(5.455));
+
+// console.log(Math.min(5,4,6,2));
+// console.log(Math.max(5,8,9,4));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log(Math.floor(Math.random() * 10) + 1 );
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); //for dice game. 
+
+
+
+
+
