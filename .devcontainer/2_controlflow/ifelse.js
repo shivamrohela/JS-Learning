1cc12377d84e094d7d7913f2d9932f8e899849a3
shivamrohela <119654333+shivamrohela@users.noreply.github.com>
2024-02-20 11:46:00 +0000

    ifelse

---
+++ .devcontainer/2_controlflow/ifelse.js
@@ -0,0 +1,36 @@
+// if (true) {
+//     console.log("you know who i am");
+// }
+
+// <,>,<=,>=,==,!=,===,!==,&&,|| for conditon we use these operator
+
+// const num = 100;
+
+// if (num>200) {
+//     console.log("yes num is greater than 200");
+// } else{
+//     console.log("num is not greater than 200");
+// }
+
+// if (2 == "2") { //here they match only value
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// if (2 === "2") {// here they match value and data type 
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+const score = 100
+const bat = "MRF"
+
+if(score >= 50 && bat == "MRF"){
+    console.log("good player");
+}
+
+if(score >= 50 || bat == "nike"){
+    console.log("good player");
+}
