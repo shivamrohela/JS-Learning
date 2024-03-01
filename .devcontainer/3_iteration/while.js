24f42a6e55a5d09215cf1cff62488df474cbd282
shivamrohela <119654333+shivamrohela@users.noreply.github.com>
2024-03-01 13:34:48 +0000

    while and do_while

---
+++ .devcontainer/3_iteration/while.js
@@ -0,0 +1,19 @@
+let i = 1;
+
+while (i<=10) {
+    console.log(i);
+    i = i + 2;
+    }
+
+let myArray = ["beer", "whisky", "rum"]
+let j = 0;
+while (j<myArray.length) {
+    console.log(myArray[j]);
+    j++;
+}
+
+let k = 2;
+do {
+    console.log(k);
+    k = k+2;
+} while (k<20);
