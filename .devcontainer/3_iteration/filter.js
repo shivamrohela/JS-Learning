fd0554328097ffc4aa265e3d164c771249c18a97
shivamrohela <119654333+shivamrohela@users.noreply.github.com>
2024-03-07 09:37:20 +0000

    filter

---
+++ .devcontainer/3_iteration/filter.js
@@ -0,0 +1,69 @@
+// const nums = [1,2,3,4,6]
+
+// const val = nums.forEach( (item) => {
+//     console.log(item);
+//     return item
+// })
+
+// console.log(val);
+
+//const num = [1,2,3,4,5,6]
+
+// const numval = num.filter( (val) => val>4)
+// console.log(numval);
+
+// const numval = num.filter( (val) => {val>4})
+// console.log(numval);
+
+// const numval = num.filter( (val) => {
+//     return val>4
+// })
+// console.log(numval);
+
+// const numarr = [];
+
+// const numval = num.forEach((val) => {
+//     if (val>3) {
+//         numarr.push(val)
+        
+//     }
+// })
+// console.log(numarr);
+
+const book = [{
+    bookname: "maths",
+    edition: 2020,
+    author: "cv raman"
+},
+{
+    bookname: "hindi",
+    edition: 2021,
+    author: "ram"
+},
+{
+    bookname: "science",
+    edition: 2016,
+    author: "akeel"
+},
+{
+    bookname: "english",
+    edition: 2019,
+    author: "shambhavi"
+},
+{
+    bookname: "english",
+    edition: 2022,
+    author: "shivam"
+}];
+
+// const books = book.filter((bk)=> bk.edition >= 2020)
+// console.log(books);
+
+// const books = book.filter((bk)=> {
+//     return bk.edition >= 2020 && bk.bookname==="english";
+// })
+// console.log(books)
+
+
+
+
